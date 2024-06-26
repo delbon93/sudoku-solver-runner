282dae243aa3b51afd6f8f530e0ea1d7ed1fd5eb
David Holzapfel <delbon93@gmail.com>
2024-06-26 22:36:36 +0200

    ISudokuSolver interface

---
+++ SudokuSolver/FailedToSolveException.cs
@@ -0,0 +1,7 @@
+﻿namespace SudokuSolver;
+
+public class FailedToSolveException(SudokuPuzzle stateAtFailure, string reason = "") : Exception {
+    
+    public SudokuPuzzle StateAtFailure { get; private set; } = stateAtFailure;
+    public string Reason { get; private set; } = reason;
+}
---
+++ SudokuSolver/ISudokuSolver.cs
@@ -0,0 +1,14 @@
+﻿namespace SudokuSolver;
+
+public interface ISudokuSolver {
+
+    /// <summary>
+    /// Solve a sudoku puzzle. If the solver fails to solve the sudoku for some reason, a <seealso cref="FailedToSolveException"/>
+    /// is thrown.
+    /// </summary>
+    /// <param name="puzzle">The sudoku puzzle state to solve</param>
+    /// <exception cref="FailedToSolveException"></exception>
+    /// <returns>The solution state of the sudoku puzzle</returns>
+    public SudokuPuzzle Solve(SudokuPuzzle puzzle);
+
+}
